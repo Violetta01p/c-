1f78f079c6a9ae50a7577d7374e5733fcf550505
Violetta <pvsergeevnaa@gmail.com>
2026-03-31 21:42:17 +0300

    pz10

--- Brand.cs
+++ Brand.cs
@@ -2,7 +2,8 @@ using System.Collections.Generic;
 
 public class Brand
 {
-    public int Id { get; set; } 
-    public string Name { get; set; } 
-    public List<Product> Products { get; set; } = new List<Product>(); 
+    public int Id { get; set; }
+    public string? Name { get; set; } 
+    public int YearFounded { get; set; }
+    public List<Product> Products { get; set; } = new List<Product>();
 }
